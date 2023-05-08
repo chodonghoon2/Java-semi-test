90930df1a70dc5fe8a710ca21bc06edda6d959b4
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2023-05-08 14:07:50 +0900

    초기 세팅

---
+++ midterm-exam-homework-chodonghoon/src/main/java/kr/ac/sahmyook/home/func/WhileSample.java
@@ -0,0 +1,4 @@
+package kr.ac.sahmyook.home.func;
+
+public class WhileSample {
+}
---
+++ midterm-exam-homework-chodonghoon/src/main/java/kr/ac/sahmyook/home/view/Menu.java
@@ -0,0 +1,42 @@
+package kr.ac.sahmyook.home.view;
+
+import kr.ac.sahmyook.home.func.*;
+public class Menu {
+    public void mainMenu() {
+        
+    }
+    public void subVarMenu() {
+
+    }
+    public void subPTypeMenu() {
+
+    }
+    public void subIfMenu() {
+
+    }
+    public void subSwitchMenu() {
+
+    }
+    public void subForMenu() {
+
+    }
+    public void subWhileMenu() {
+
+    }
+    public void subDoWhileMenu() {
+
+    }
+    public void subBreakMenu() {
+
+    }
+    public void subContinueMenu() {
+
+    }
+    public void subStaticMethodMenu() {
+
+    }
+    public void subNonStaticMethodMenu() {
+
+    }
+}
+
